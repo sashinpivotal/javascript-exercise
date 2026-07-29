5909115de4f196a01cd50aa04938d34a6ae985ba
Sang Shin <sangshinpassion@gmail.com>
2026-07-29 19:21:21 -0400

    async exercises are cleaned up

--- async-await-2.js
+++
@@ -1,7 +0,0 @@
-async function getDate() {
-  const response = await fetch('https://timeapi.io/api/time/current/zone?timeZone=America/New_York');
-  const data = await response.json();
-  console.log(data);
-}
-
-getDate();
