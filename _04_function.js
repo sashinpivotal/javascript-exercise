33ef3850280e2905bd1c77f1109441c09a59f9d9
Sang Shin <sangshinpassion@gmail.com>
2026-07-29 21:27:17 -0400

    some change

--- _04_function.js
+++ _04_function.js
@@ -1,9 +1,11 @@
 function constructMessage(firstName, lastName) {
     return "Hello " + firstName + " " + lastName;
 }
+
 function displayMessage(firstName, lastName) {
     let msg = constructMessage(firstName, lastName);
     console.log(msg);
 }
-constructMessage("Nick", "Todd");
-displayMessage("Nick", "Todd");
+
+constructMessage("John", "Spellman");
+displayMessage("John", "Spellman");
