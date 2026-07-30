aacb7abe37824d08d05cbb0c768881683f468f2c
Sang Shin <sangshinpassion@gmail.com>
2026-07-30 12:31:09 -0400

    modify scoping code

--- _05_scoping.js
+++ _05_scoping.js
@@ -1,29 +1,33 @@
 // Variables declared without use of let are 
 // always global regardless of where you declare them
-function sayHello(name) {
+function sayHello1(name) {
     console.log(name)
     anotherGlobalVariable = name;
 }
 
 globalNameVariable = "Nick"
-sayHello(globalNameVariable)
+sayHello1(globalNameVariable)
 console.log(anotherGlobalVariable)
 
 // Variables declared with var are function scoped
 // - var is discouraged in modern JavaScript mainly because of 
 //   this scoping quirk that could cause subtle bugs. 
 // - let and const (introduced in ES6/2015) fix these problems
-function sayHello(name) {
+function sayHello2(name) {
     for (var i = 0; i < 3; i++) {
-        console.log(i)
+        console.log("sayHello2: " + i)
     }
-    console.log(i) // does this work? It does!
+    console.log("sayHello2: " + i) // does this work? It does!
 }
 
+sayHello2("Nick");
+
 // Variables declared with let are block scoped
-function sayHello(name) {
+function sayHello3(name) {
     for (let i = 0; i < 3; i++) {
-        console.log(i)
+        console.log("sayHello3: " + i)
     }
-    console.log(i) // does this work? Not any more
+    console.log("sayHello2: " + i) // does this work? Not any more
 }
+
+sayHello3("Nick");
