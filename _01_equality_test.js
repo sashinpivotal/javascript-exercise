79666126832b94ec1f0a85ef4f5d15135988075f
Sang Shin <sangshinpassion@gmail.com>
2026-08-02 09:23:56 -0400

    add info. on Code Runner

--- _01_equality_test.js
+++ _01_equality_test.js
@@ -1,3 +1,10 @@
+// You should be able to run the JavaScript codes
+// in this directory by running it within the
+// VSC assuming you have installed Code Runner extension.
+// The console.log(..) statements will print to the 
+// Output window of VSC.
+
+// Equality and Inequality Operators
 console.log(5 == "5")
 console.log(5 === "5")
 console.log(5 != "5")
